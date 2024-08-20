ff7beb80fcfc94093d6aeaa903b5e452fb81aacc
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-08-19 19:18:21 -0700

    Add method to generate a random word

---
+++ lib/game.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+# Manage game state, save and load game
+class Game
+  DICTIONARY = 'google-10000-english'
+  def intialize
+  end
+
+  def self.random_word
+    return unless File.exist? DICTIONARY
+
+    File.open(DICTIONARY).each.filter do |word|
+      word.strip.length.between?(5, 12)
+    end.sample.strip
+  end
+end
