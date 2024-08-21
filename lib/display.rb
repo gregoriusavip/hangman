62521fa3737e7330a96d1be8f210bb59f1a17eab
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-08-21 16:18:25 -0700

    Refactor for readability

--- lib/display.rb
+++ lib/display.rb
@@ -2,17 +2,10 @@
 
 # Display prompts/informations to the terminal
 class Display
-  def self.guess_prompt(guess_limit)
-    puts "You have #{guess_limit} remaining guess(es)"
-  end
-
-  def self.chosen_letters(correct_letters, incorrect_letters)
-    puts "Correct chosen letters: #{correct_letters.to_a}"
-    puts "Incorrect chosen letters: #{incorrect_letters.to_a}"
-  end
-
-  def self.hangman_progress(progress)
-    puts progress.join(' ')
+  def self.hangman_prompt_loop(guess_limit, correct_letters, incorrect_letters, progress)
+    guess_prompt(guess_limit)
+    chosen_letters(correct_letters, incorrect_letters)
+    hangman_progress(progress)
   end
 
   def self.announce_win(answer)
@@ -23,5 +16,18 @@ class Display
   def self.announce_lose(answer)
     puts 'You ran out of guess!'
     puts "The secret word is: #{answer}"
+  end
+
+  def self.hangman_progress(progress)
+    puts progress.join(' ')
+  end
+
+  def self.guess_prompt(guess_limit)
+    puts "You have #{guess_limit} remaining guess(es)"
+  end
+
+  def self.chosen_letters(correct_letters, incorrect_letters)
+    puts "Correct chosen letters: #{correct_letters.to_a}"
+    puts "Incorrect chosen letters: #{incorrect_letters.to_a}"
   end
 end
