5020a27f6e4eb2b3df48ee9ef8d033052e0f58ce
gregoriusavip <71865513+gregoriusavip@users.noreply.github.com>
2024-08-20 19:44:22 -0700

    Convert game to module

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Manage game state, save and load game
-class Game
+module Game
   DICTIONARY = 'google-10000-english'
 
   def self.random_word
@@ -9,6 +9,6 @@ class Game
 
     File.open(DICTIONARY).each.filter do |word|
       word.strip.length.between?(5, 12)
-    end.sample
+    end.sample.chomp
   end
 end
